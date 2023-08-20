79dc1b7765470833ca93456203260afeb7480646
MaranoMMV <matheusmmv14@gmail.com>
2023-08-20 17:38:32 -0300

    Adicionado um comando manual no Vendas Application

--- src/main/java/br/com/maranoart/VendasApplication.java
+++ src/main/java/br/com/maranoart/VendasApplication.java
@@ -1,8 +1,5 @@
 package br.com.maranoart;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,39 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import br.com.maranoart.domain.entity.Cliente;
-import br.com.maranoart.domain.entity.Pedido;
 import br.com.maranoart.domain.repository.Clientes;
-import br.com.maranoart.domain.repository.Pedidos;
 
 @SpringBootApplication
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init( @Autowired Clientes clientes, @Autowired Pedidos pedidos) {
-        return args -> {
-            System.out.println("Salvando clientes");
-            Cliente fulano = new Cliente("Fulano");
-            clientes.save(fulano);
-
-            Pedido p = new Pedido();
-            p.setCliente(fulano);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-        //    Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
-        //    System.out.println(cliente);
-        //    System.out.println(cliente.getPedidos());
-
-           pedidos.findByCliente(fulano).forEach(System.out::println);
-
-
-
-
+    public CommandLineRunner commandLineRunner(@Autowired Clientes cliente){
+        return args ->{
+            Cliente c = new Cliente(null, "fulano");
+            cliente.save(c);
         };
     }
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
--- src/main/java/br/com/maranoart/rest/controller/ClienteController.java
+++ src/main/java/br/com/maranoart/rest/controller/ClienteController.java
@@ -1,27 +1,43 @@
 package br.com.maranoart.rest.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.maranoart.domain.entity.Cliente;
+import br.com.maranoart.domain.repository.Clientes;
 
 @Controller
 // @RequestMapping("/api/clientes")
 public class ClienteController {
-    
-    @RequestMapping(
-        value = {"/ api/clientes/hello/{nome}", "/api/hello"},
-        method = RequestMethod.POST,
-        consumes = { "application/json", "application/xml" },
-        produces = { "application/json", "application/xml"}
-    )
+
+    //Atributos
+    private Clientes clientes;
+
+
+    //Construtor
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+
+    //Métodos
+    @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cllente){
-        return String.format("Hello %s", nomeCliente);
+    public ResponseEntity getClienteById( @PathVariable Integer id ){
+
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
+
     }
 
 }
