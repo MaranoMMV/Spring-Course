5c051127317bb6418d40cb9f474862e206809132
MaranoMMV <matheusmmv14@gmail.com>
2023-08-20 21:34:28 -0300

    Adicionado os metodos Post e Get no HTTP

--- src/main/java/br/com/maranoart/VendasApplication.java
+++ src/main/java/br/com/maranoart/VendasApplication.java
@@ -1,24 +1,10 @@
 package br.com.maranoart;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import br.com.maranoart.domain.entity.Cliente;
-import br.com.maranoart.domain.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
-
-    @Bean
-    public CommandLineRunner commandLineRunner(@Autowired Clientes cliente){
-        return args ->{
-            Cliente c = new Cliente(null, "fulano");
-            cliente.save(c);
-        };
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
--- src/main/java/br/com/maranoart/rest/controller/ClienteController.java
+++ src/main/java/br/com/maranoart/rest/controller/ClienteController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.maranoart.domain.entity.Cliente;
@@ -40,4 +42,10 @@ public class ClienteController {
 
     }
 
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save(@RequestBody Cliente cliente ){
+        Cliente ClienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(ClienteSalvo);
+    }
 }
