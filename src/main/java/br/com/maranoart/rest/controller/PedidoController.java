a383d79ae43f4ae76b93da7f84080dd08772179f
MaranoMMV <matheusmmv14@gmail.com>
2023-08-21 11:55:18 -0300

    Realizado o PedidoController para API Rest

---
+++ src/main/java/br/com/maranoart/rest/controller/PedidoController.java
@@ -0,0 +1,12 @@
+package br.com.maranoart.rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidoController {
+
+    
+    
+}
