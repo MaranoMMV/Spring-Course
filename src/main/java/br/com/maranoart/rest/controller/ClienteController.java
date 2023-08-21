e98e00cb40cec03b372f70850bd0bc85f6a264f9
MaranoMMV <matheusmmv14@gmail.com>
2023-08-20 22:01:52 -0300

    Adicionado o a requisicao PUT

--- src/main/java/br/com/maranoart/rest/controller/ClienteController.java
+++ src/main/java/br/com/maranoart/rest/controller/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -62,4 +63,21 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
 
     }
+
+    @PutMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity update( @PathVariable Integer id,
+                                  @RequestBody Cliente cliente )
+    {
+
+        return clientes
+            .findById(id)
+            .map( clienteExistente -> {
+                cliente.setId(clienteExistente.getId());
+                clientes.save(cliente);
+                return ResponseEntity.noContent().build();
+        }).orElseGet( () -> ResponseEntity.notFound().build() );
+
+    }
+
 }
