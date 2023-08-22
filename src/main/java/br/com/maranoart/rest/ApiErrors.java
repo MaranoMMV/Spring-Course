f71477db4bad310b0fec5e067188eece2ce0918f
MaranoMMV <matheusmmv14@gmail.com>
2023-08-22 15:26:14 -0300

    API Errors

---
+++ src/main/java/br/com/maranoart/rest/ApiErrors.java
@@ -0,0 +1,18 @@
+package br.com.maranoart.rest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class ApiErrors {
+    
+    private List<String> errors;
+
+    public ApiErrors(String mensagemErro){
+        this.errors = Arrays.asList(mensagemErro);
+    }
+
+
+}
---
+++ src/main/java/br/com/maranoart/rest/ApplicationControllerAdvice.java
@@ -0,0 +1,20 @@
+package br.com.maranoart.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.maranoart.exception.RegraNegocioException;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+    
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex){
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+
+}
