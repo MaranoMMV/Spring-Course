a604d795d134779d2d713632166629b5c2c9b28f
MaranoMMV <matheusmmv14@gmail.com>
2023-08-30 17:46:58 -0300

    Adicionado Spring Security

---
+++ src/main/java/br/com/maranoart/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package br.com.maranoart.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+
+        };
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+        .passwordEncoder(passwordEncoder())
+        .withUser("fulano")
+        .password(passwordEncoder().encode("123"))
+        .roles("USER");
+        
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        super.configure(http);
+    }
+    
+}
